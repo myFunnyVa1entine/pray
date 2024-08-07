9155f28467d9da742e5eae8a6f6de2f837d624ec
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-07 10:29:25 +0900

    DESIGN : Main Img nChanged

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <a href='/programs' class='flex justify-center items-center h-screen'>
       <image
         class='h-2/3'
-        src='https://file.miricanvas.com/template_thumb/2024/07/02/18/40/k2udxw55w6620kcq/thumb.webp?size=800' alt='main poster' />
+        src='/webposter.webp' alt='main poster' />
     </a>
     <Mode />
   </main>
