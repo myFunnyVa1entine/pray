6d69dd0b4fb682994b89495e11bd454e2144f573
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-16 00:03:59 +0900

    STYLE : Dark Mode

--- routes/(pages)/artists/index.tsx
+++ routes/(pages)/artists/index.tsx
@@ -1,7 +1,7 @@
 import Card from "../../../components/Card.tsx";
 
 export default function Home() {
-  return <section class='layout text-center h-screen-body dark:text-white dark:bg-black'>
+  return <section class='layout text-center overflow-auto dark:text-white dark:bg-black'>
     <Card />
     <Card />
     <Card />
