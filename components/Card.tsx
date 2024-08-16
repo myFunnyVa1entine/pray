27a777d794686fecfe8812e827982dc79c096df0
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-17 00:27:27 +0900

    DOCS : Change Cards Specific Files

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,28 @@
+import my_artists from '../static/my_artists.json' with { type: "json" };
+interface IArtists {
+  src: string
+  name: {
+    kor: string
+    eng: string
+  },
+  desc?: string
+}
+
+const artists: IArtists[] = my_artists;
+
 export default function Card() {
   return <section class='overflow-auto'>
-    <div class='m-4 p-4'>
-      <div class='flex gap-4 items-end'>
-        <h2 class='text-[1.5rem]'>작가 이름{ } artist name</h2>
-      </div>
-      <div class='w-full h-[200px] flex justify-center items-center bg-purple-200 my-4'>
-        작가 사진
-      </div>
-      <div class='text-start'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, impedit recusandae eveniet expedita fugiat nisi! Similique enim officia porro illo maiores sed nobis quia reiciendis facere laborum, dicta suscipit ut.
-      Unde non corporis atque labore, tempore recusandae et alias possimus laudantium ad, reiciendis minus nisi debitis numquam fugiat nam voluptatem voluptate porro aliquid at commodi veritatis assumenda error. Eligendi, tenetur?</div>
+    {artists.map((artist:IArtists) => {
+      return <div class='m-4 p-4' key={artist.src}>
+        <div class='flex gap-4 items-end'>
+          <h2 class='text-[1.5rem]'>{artist.name.kor}{' '}{artist.name.eng}</h2>
+        </div>
+        <div class='w-full h-auto flex justify-center items-center my-4'>
+          <img src={`/JH_artists/${artist.src}.jpeg`} class='w-full object-contain rounded-xl' alt={artist.name.eng} />
+        </div>
+        <div class='text-start'>{artist.desc ? <em>{artist.desc}</em> : '-'}
+        </div>
     </div>
+    })}
   </section>
 }
--- routes/(pages)/artists/index.tsx
+++ routes/(pages)/artists/index.tsx
@@ -3,11 +3,11 @@ import Card from "../../../components/Card.tsx";
 export default function Home() {
   return <section class='layout text-center overflow-auto dark:text-white dark:bg-black'>
     <Card />
+    {/* <Card />
     <Card />
     <Card />
     <Card />
     <Card />
-    <Card />
-    <Card />
+    <Card /> */}
   </section>
 }
