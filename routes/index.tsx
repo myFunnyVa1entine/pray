9ad719aec155f903bac12f8e513e5da3954b00a1
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-18 19:08:44 +0900

    DESIGN : Main Design Changed

--- routes/index.tsx
+++ routes/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return <main class='dark:bg-black  w-screen h-screen antialiased duration-300'>
     <a href='/introduce' class='flex justify-center items-center h-screen'>
       <image
-        class='w-auto h-2/3'
+        class='w-2/3 h-auto'
         src='/brochure.gif' alt='main poster' />
     </a>
     <Mode />
