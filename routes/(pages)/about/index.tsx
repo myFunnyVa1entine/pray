16f6c824f9f5eb83560191622961f29bb27af5d5
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-18 23:32:31 +0900

    DOCS : Add Contents On Pages

--- routes/(pages)/about/index.tsx
+++ routes/(pages)/about/index.tsx
@@ -1,6 +1,16 @@
 export default function Home() {
-  const hi = 'layout success!'
-  return <section class='layout text-center h-screen-body dark:text-white dark:bg-black'>
-    about page {hi}
+  return <section class='layout h-screen-body dark:text-white dark:bg-black '>
+    <article class='p-4 font-gosha'>
+      <h1 class='text-lg'>모험하는 아티스트와 콜렉터들의 놀이터</h1>
+      <h3 class='text-xl'>페이퍼 그라운드</h3>
+    < br/>
+    <img src='/mini_logo.webp' alt='poster image' class='w-52 py-4' />
+    < br/>
+    페이퍼그라운드는 동시대 예술가들의 작업물을 다양한 루트로 선보이고 콜렉터와 연결하는 아트 커머스 에이전시(Art Commerce Agency)이자 온라인 편집샵(Collect Shop) 입니다.  
+    자신만의 독특한 시선으로 작업을 이어가는 아티스트들, 그리고 이들의 시선과 작품에 공감하는 콜렉터들에게 페이퍼그라운드는 즐거운 놀이터이자 연결의 플랫폼이 되고자 합니다.
+    단순한 소비를 뛰어넘어 시선을 공유하고 교감하는 새로운 일상 예술 문화를 함께 만들어가기를 제안합니다.
+
+    <img src='/insta_qr.png' alt='poster image' class='w-[120px] py-4' />
+    </article>
   </section>
 }
--- routes/(pages)/party/index.tsx
+++ routes/(pages)/party/index.tsx
@@ -1,5 +1,43 @@
 export default function Hone() {
-  return <section class='layout text-center h-screen-body dark:text-white dark:bg-black'>
-    hi this is party page
+  return <section class='layout h-screen-body dark:text-white dark:bg-black overflow-auto p-4'>
+    <h1 class='text-[1.5rem]'>전시 오프닝</h1>
+    <h3 class='text-[1rem] font-gosha'>OPENING CEREMONY</h3>
+    <article class='py-4'>
+      <p>서울을 베이스로 활동하는 페이퍼 그라운드.</p>
+      <p>장흥을 베이스로 활동하는 아브리 아뜰리에.</p>
+      <p>이 두 곳과 연을 가지고 활동중인 예술가/문화예술기획자 친구들의 네트워크 파티이자 공연으로 진행됩니다.</p>  
+    </article>
+
+    <article class='py-4 font-gosha'>
+      <p>일시 및 장소 DATE & LOCATION</p>
+    </article>
+
+    <article>
+      <p>2024년 9월 1일 5PM</p>
+      <p>아브리 아뜰리에</p>
+    </article>
+
+    <p class='pb-4 pt-8 font-gosha'>타임라인 TIMELINE</p>
+    <ol class='-z-10 relative border-l border-[#abaaed] pl-5 ml-2 last:border-0 last:pb-0 list:none my-2'>
+      <li class="absolute -left-2 top-1 w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
+      <li class="absolute -left-2 top-[7.75rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
+      <li class="absolute -left-2 top-[15.25rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
+      <li>전시 소개 및 인사</li>
+      <li>오프닝 퍼포먼스 | 이지예 (뭐라고 적을까?)</li>
+      <li>전시 소개 | 임나은 (페이퍼 그라운드 대표)</li>
+      <li>아뜰리에 소개 | 김미진 (아브리 아뜰리에 대표)</li>
+      <br />
+      <li>식사 및 공연</li>
+      <li>공연 1 | 싱어송라이터 이지영</li>
+      <li>공연 2 |소리 박운종 & 고수 이명식</li>
+      <li>공연 3 | 디제이 데이즈 보틀</li>
+      <br />
+      <li>네트워킹</li>
+    </ol>
+    <footer class='pt-6'>
+      <p>주최•기획 | 아브리 아뜰리에 x 페이퍼 그라운드</p>
+      <p>진행 | 이지예</p>
+    </footer>
+    
   </section>
 }
