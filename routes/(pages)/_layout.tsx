46a22f6bda88e03b4b9c63bf97c9606d009fa749
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-21 22:32:51 +0900

    DEL : Delete Mode Change Code

--- routes/(pages)/_layout.tsx
+++ routes/(pages)/_layout.tsx
@@ -1,14 +1,11 @@
 import { PageProps } from "$fresh/server.ts";
 import Menus from "../../islands/Menus.tsx";
-import Mode from '../../islands/Mode.tsx';
-
 
 export default function Layout({ Component }: PageProps) {
   return (
-    <div class='dark:text-white dark:bg-black'>
+    <div class='text-white bg-black'>
       <Menus />
       <Component />
-      <Mode />
     </div>
   );
 }
