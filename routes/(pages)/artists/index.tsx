8954eb4851ab97c82dd603d7dc268627f6211c5d
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-22 22:59:22 +0900

    DESIGN : Resize Artists Image

--- routes/(pages)/artists/index.tsx
+++ routes/(pages)/artists/index.tsx
@@ -10,7 +10,7 @@ interface IInfo {
 function Card({ info }: { info: IInfo }) {
   return <article class='flex flex-col gap-3 pb-12'>
     <h2 class='text-[1.5rem]'>{info.kor_name} {info.eng_name}</h2>
-    <img src={`/JH_artists/${info.eng_name.split(' ').join('').toLowerCase()}.jpeg`} alt={info.eng_name} class={`h-[400px] rounded ${info.w !== undefined ? 'w-2/3' : 'w-full'}`} />
+    <img src={`/JH_artists/${info.eng_name.split(' ').join('').toLowerCase()}.jpeg`} alt={info.eng_name} class={`rounded ${info.w !== undefined ? 'w-1/2 h-[220px]' : 'h-[320px] w-full'}`} />
     {info.desc && <em>{info.desc}</em>}
 </article>
 }
