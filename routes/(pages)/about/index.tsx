3baddca52a804e0250f78012631dcf7e729c749b
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-22 22:33:23 +0900

    DOCS : Fix Codes

--- routes/(pages)/about/index.tsx
+++ routes/(pages)/about/index.tsx
@@ -1,29 +1,18 @@
-export default function Home() {
-  return <section class='layout h-screen dark:text-white dark:bg-black '>
-    <article class='p-4'>
-      <h1 class='text-lg'>모험하는 아티스트와 콜렉터들의 놀이터</h1>
-      <h3 class='text-xl'>페이퍼 그라운드</h3>
-      < br/>
-      <img src='/mini_logo.webp' alt='poster image' class='w-52 py-4' />
-      < br/>
-      페이퍼그라운드는 동시대 예술가들의 작업물을 다양한 루트로 선보이고 콜렉터와 연결하는 아트 커머스 에이전시(Art Commerce Agency)이자 온라인 편집샵(Collect Shop) 입니다.  
-      자신만의 독특한 시선으로 작업을 이어가는 아티스트들, 그리고 이들의 시선과 작품에 공감하는 콜렉터들에게 페이퍼그라운드는 즐거운 놀이터이자 연결의 플랫폼이 되고자 합니다.
-      단순한 소비를 뛰어넘어 시선을 공유하고 교감하는 새로운 일상 예술 문화를 함께 만들어가기를 제안합니다.
+import exhibitors from '../../../static/exhibitors.json' with { type: "json" }
 
-      <a href="https://www.instagram.com/paperground.kr/">
-        <img src='/JH_artists/insta.png' alt='poster image' class='w-[28px] py-4 hover:cursor-pointer' />
-      </a>
-    </article>
-    <article class='p-4'>
-      <h1 class='text-lg'>아브리 아뜰리에</h1>
-      <h3 class='text-xl'>Abri Atelier</h3>
-      < br/>
-      <img src='JH_artists/aburi.png' alt='poster image' class='w-52 py-4' />
-      < br/>
-      아브리 아뜰리에는 불어로 안식처, 작업실이란 의미를 담고 있는 갤러리이자 미술 체험 공간입니다. 전라남도 장흥에 처음으로 생긴 Contemporary Art Gallery (동시대 예술 갤러리)로서 현존하는 작가들의 작품을 전시하는 공간입니다. 강아지 삼 형제와 아기 고양이가 함께 살고 있는 작은 카오스의 공간 아브리 아뜰리에는 2023년 9월에 오픈하여 지역민들에게 오늘날의 미술을 소개하고, 타 지역 예술가들과 만나는 현재진행형 공간입니다.아브리(안식처)라는 말이 나타내듯 방문하는 모든 분들에게 편안한 쉼의 공간이 되기를 지향합니다.
-      <a href="https://www.instagram.com/abriatelier/">
-        <img src='/JH_artists/insta.png' alt='poster image' class='w-[28px] py-4 hover:cursor-pointer' />
-      </a>
-    </article>
+export default function Home() {
+  return <section class='layout h-auto dark:text-white dark:bg-black pl-6 pr-[22px] pt-16 pb-32'>
+    {exhibitors.map(exhibitor => {
+      return <article key={exhibitor.name.eng} class='pb-12'>
+        <h1 class='text-lg'>{exhibitor.title}</h1>
+        <h3 class='text-xl'>{exhibitor.name.kor}</h3>
+        <img src={exhibitor.src} alt='poster image' class='w-52 py-10' />
+        <p>{exhibitor.desc}</p>
+        <a href={exhibitor.href} target='_blank'>
+          <img src='/JH_artists/insta.png' alt='poster image' class='w-[28px] pt-4 hover:cursor-pointer' />
+        </a>
+      </article>
+    })
+  }
   </section>
 }
--- routes/(pages)/artists/index.tsx
+++ routes/(pages)/artists/index.tsx
@@ -1,30 +1,22 @@
-import my_artists from '../../../static/my_artists.json' with { type: "json" };
+import artists from '../../../static/artists.json' with { type: "json" }
 
-interface IArtists {
-  src: string
-  name: {
-    kor: string
-    eng: string
-  },
+interface IInfo {
+  kor_name: string
+  eng_name: string
+  w?: number,
   desc?: string
 }
 
-const artists: IArtists[] = my_artists;
+function Card({ info }: { info: IInfo }) {
+  return <article class='flex flex-col gap-3 pb-12'>
+    <h2 class='text-[1.5rem]'>{info.kor_name} {info.eng_name}</h2>
+    <img src={`/JH_artists/${info.eng_name.split(' ').join('').toLowerCase()}.jpeg`} alt={info.eng_name} class={`h-[400px] rounded ${info.w !== undefined ? 'w-[300px]' : 'w-full'}`} />
+    {info.desc && <em>{info.desc}</em>}
+</article>
+}
 
-export default function Home() {
-  return <section class='layout h-auto text-center dark:text-white dark:bg-black font-gosha'>
-    {artists.map((artist:IArtists) => {
-      return <div class='px-6 py-4' key={artist.src}>
-        <div class='flex gap-4 items-end'>
-          <h2 class='text-[1.5rem]'>{artist.name.kor}{' '}{artist.name.eng}</h2>
-        </div>
-        <div class='w-full h-auto flex justify-center items-center my-4'>
-          <img src={`/JH_artists/${artist.src}.jpeg`} class='w-full object-contain rounded' alt={artist.name.eng} />
-        </div>
-        <div class='text-start mb-10'>
-          {artist.desc ? <em>{artist.desc}</em> : ''}
-        </div>
-    </div>
-    })}
+export default function ArtistPage() {
+  return <section class='layout text-white bg-black px-6 pt-16 pb-32'>
+    {artists.map((artist: IInfo) => <Card info={artist} /> )}
   </section>
 }
