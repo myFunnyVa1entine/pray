26472255de2ebc4a75a07c63b785aeace939de01
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-24 22:01:17 +0900

    DOCS : Fix Artist Introduce Info

--- routes/(pages)/artists/index.tsx
+++ routes/(pages)/artists/index.tsx
@@ -4,14 +4,29 @@ interface IInfo {
   kor_name: string
   eng_name: string
   w?: number,
-  desc?: string
+  desc?: string,
+  home?: string,
+  instagram?: string
 }
 
 function Card({ info }: { info: IInfo }) {
   return <article class='flex flex-col gap-3 pb-12'>
     <h2 class='text-[1.5rem]'>{info.kor_name} {info.eng_name}</h2>
-    <img src={`/JH_artists/${info.eng_name.split(' ').join('').toLowerCase()}.jpeg`} alt={info.eng_name} class={`rounded ${info.w !== undefined ? 'w-1/2 h-[220px]' : 'h-[320px] w-full'}`} />
+    <img src={`/JH_artists/${info.eng_name.split(' ').join('').toLowerCase()}.jpeg`} alt={info.eng_name} class={`rounded ${info.w !== undefined ? 'w-1/2' : 'w-[70%]'}`} />
     {info.desc && <em>{info.desc}</em>}
+    
+    <div class='flex gap-2 items-center'>
+      {
+        info.home && <a href={`https://${info.home}`} target='_blank'>
+          <img src="/JH_artists/home.png" alt={`${info.home}'s homepage`} class='w-[26px] h-[26px]' />
+        </a>
+      }
+      {
+        info.instagram && <a href={`https://www.instagram.com/${info.instagram}`} target='_blank'>
+          <img src="/JH_artists/instagram.png" alt={`${info.instagram}'s instagram`} class='w-7 h-7' />
+        </a>
+      }
+    </div>
 </article>
 }
 
