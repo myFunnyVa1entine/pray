8392208786fb4e61d13c4d922367997a2bb806b6
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-24 22:06:45 +0900

    DESIGN : Fix Poster Size

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return <main class='bg-black  antialiased duration-300'>
     <a href='/introduce' class='flex justify-center items-center h-screen'>
       <image
-        class='w-auto h-auto sm:w-[640px] px-12'
+        class='w-[88%] h-auto sm:w-[640px]'
         src='/poster.gif' alt='main poster' />
     </a>
   </main>
