81dcff8749f02ba9b95227ce0038b8595e607266
myFunnyVa1entine <brilliant2102@gmail.com>
2024-08-25 21:17:13 +0900

    DOCS : Fix Party Page

--- routes/(pages)/party/index.tsx
+++ routes/(pages)/party/index.tsx
@@ -20,17 +20,17 @@ export default function Hone() {
     <p class='pb-4 pt-8'>타임라인 TIMELINE</p>
     <ol class='z-0 relative border-l border-[#abaaed] pl-5 ml-2 last:border-0 last:pb-0 list:none my-2'>
       <li class="absolute -left-2 top-1 w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
-      <li class="absolute -left-2 top-[7.75rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
+      <li class="absolute -left-2 top-[6.25rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
       <li class="absolute -left-2 top-[15.25rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
       <li class='text-white'>전시 소개 및 인사</li>
-      <li>오프닝 퍼포먼스 | 이지예 (스토리 텔러)</li>
       <li>전시 소개 | 임나은 (페이퍼 그라운드 대표)</li>
       <li>아뜰리에 소개 | 김미진 (아브리 아뜰리에 대표)</li>
       <br />
       <li>식사 및 공연</li>
       <li>공연 1 | 싱어송라이터 이지영</li>
-      <li>공연 2 | 소리 박운종 & 고수 이명식</li>
-      <li>공연 3 | 디제이 데이즈 보틀</li>
+      <li>공연 2 | 이지예 (스토리텔러)</li>
+      <li>공연 3 | 소리 박운종 & 고수 이명식</li>
+      <li>공연 4 | 디제이 데이즈 보틀</li>
       <br />
       <li>네트워킹 타임</li>
     </ol>
