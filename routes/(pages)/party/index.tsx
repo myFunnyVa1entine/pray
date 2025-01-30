4fafe3a138186c81058944d80f4563092e6769fe
myFunnyVa1entine <brilliant2102@gmail.com>
2025-01-30 15:05:52 +0900

    DOCS : Fix Code

--- routes/(pages)/party/index.tsx
+++ routes/(pages)/party/index.tsx
@@ -1,42 +1,47 @@
 export default function Hone() {
-  return <section class='layout h-auto text-white bg-black px-6 pt-16 pb-32'>
-    <h1 class='text-[1.5rem]'>전시 오프닝</h1>
-    <h3 class='text-[1rem]'>OPENING CEREMONY</h3>
-    <article class='py-4'>
-      <p>서울을 베이스로 활동하는 페이퍼 그라운드.</p>
-      <p>장흥을 베이스로 활동하는 아브리 아뜰리에.</p>
-      <p>이 두 곳과 연을 가지고 활동중인 예술가/문화예술기획자 친구들의 네트워크 파티이자 공연으로 진행됩니다.</p>  
-    </article>
+  return (
+    <section class="layout h-auto text-white bg-black px-6 pt-16 pb-32">
+      <h1 class="text-[1.5rem]">전시 오프닝</h1>
+      <h3 class="text-[1rem]">OPENING CEREMONY</h3>
+      <article class="py-4">
+        <p>서울을 베이스로 활동하는 페이퍼 그라운드.</p>
+        <p>장흥을 베이스로 활동하는 아브리 아뜰리에.</p>
+        <p>
+          이 두 곳과 연을 가지고 활동중인 예술가/문화예술기획자 친구들의
+          네트워크 파티이자 공연으로 진행됩니다.
+        </p>
+      </article>
 
-    <article class='py-4'>
-      <p>일시 및 장소 DATE & LOCATION</p>
-    </article>
+      <article class="py-4">
+        <p>일시 및 장소 DATE & LOCATION</p>
+      </article>
 
-    <article>
-      <p>2024년 9월 1일 5PM</p>
-      <p>아브리 아뜰리에</p>
-    </article>
+      <article>
+        <p>2024년 9월 1일 5PM</p>
+        <p>아브리 아뜰리에</p>
+      </article>
 
-    <p class='pb-4 pt-8'>타임라인 TIMELINE</p>
-    <ol class='z-0 relative border-l border-[#abaaed] pl-5 ml-2 last:border-0 last:pb-0 list:none my-2'>
-      <li class="absolute -left-2 top-1 w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
-      <li class="absolute -left-2 top-[6.25rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
-      <li class="absolute -left-2 top-[15.25rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
-      <li class='text-white'>전시 소개 및 인사</li>
-      <li>전시 소개 | 임나은 (페이퍼 그라운드 대표)</li>
-      <li>아뜰리에 소개 | 김미진 (아브리 아뜰리에 대표)</li>
-      <br />
-      <li>식사 및 공연</li>
-      <li>공연 1 | 싱어송라이터 이지영</li>
-      <li>공연 2 | 이지예 (스토리텔러)</li>
-      <li>공연 3 | 소리 박운종 & 고수 이명식</li>
-      <li>공연 4 | 디제이 데이즈 보틀</li>
-      <br />
-      <li>네트워킹 타임</li>
-    </ol>
-    <footer class='py-6 pb-32'>
-      <p>주최•기획 | 아브리 아뜰리에 x 페이퍼 그라운드</p>
-      <p>진행 | 이지예</p>
-    </footer>
-  </section>
+      <p class="pb-4 pt-8">타임라인 TIMELINE</p>
+      <ol class="z-0 relative border-l border-[#abaaed] pl-5 ml-2 last:border-0 last:pb-0 list:none my-2">
+        <li class="absolute -left-2 top-1 w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
+        <li class="absolute -left-2 top-[6.25rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
+        <li class="absolute -left-2 top-[15.25rem] w-4 h-4 bg-white border border-[#4e5ed3] rounded-full shadow-[3px_3px_0px_#bab5f8]" />
+        <li class="text-white">전시 소개 및 인사</li>
+        <li>전시 소개 | 임나은 (페이퍼 그라운드 대표)</li>
+        <li>아뜰리에 소개 | 김미진 (아브리 아뜰리에 대표)</li>
+        <br />
+        <li>식사 및 공연</li>
+        <li>공연 1 | 싱어송라이터 이지영</li>
+        <li>공연 2 | 이지예 (스토리텔러)</li>
+        <li>공연 3 | 소리 박운종 & 고수 이명식</li>
+        <li>공연 4 | 디제이 데이즈 보틀</li>
+        <br />
+        <li>네트워킹 타임</li>
+      </ol>
+      <footer class="py-6 pb-32">
+        <p>주관•주최 | 아브리 아뜰리에 x 페이퍼 그라운드</p>
+        <p>진행 | 이지예</p>
+      </footer>
+    </section>
+  );
 }
